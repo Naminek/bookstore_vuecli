da30f14d0cf814d2866fb60cf279b0d2b77d8691
Naminek <nam.k.0519@gmail.com>
2018-11-01 16:53:40 +0100

    got the data

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
 	<div id="app">
-    <!-- <img alt="logo1" src="../assets/red.png">
-    <img alt="logo2" src="../assets/gray.png">
-    <img alt="logo3" src="../assets/blue.png"> -->
-    <Book :books="bookData"/>
+    <div v-if="isLoading">
+      <!-- <img alt="loading" src="../assets/image_now_loading.png"> -->
+      <p>is loading...</p>
+    </div>
+    <div v-slse>
+      <header />
+      <Book :books="bookData"/>
 		<!-- <div id="nav">
 			<router-link to="/">Home</router-link> |
 			<router-link to="/about">About</router-link>
 		</div>
 		<router-view /> -->
+    </div>
 	</div>
 </template>
 
 <script>
 import Book from "@/components/Book.vue";
+import Header from "@/components/Header.vue";
 export default {
   components: {
-    Book
+    Book,
+    Header
   },
   data() {
     return {
