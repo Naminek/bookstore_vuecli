a1c4ecb51a30ffd0b5e060261d5a8b248781082f
Naminek <nam.k.0519@gmail.com>
2018-11-02 12:19:20 +0100

    did flip staff

--- src/components/OneBookData.vue
+++ src/components/OneBookData.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
   <div v-for="(oneBook, index) in oneBookData" :key="index"> 
-    <img alt="book_cover[[index]]" src="oneBook.cover">
+    <img alt="book_cover" :src="oneBook.cover">
     <div class="caption">
     	<h3 class="book_title">{{oneBook.title}}</h3>
     	<p class="book_detail">{{oneBook.description}}</p>
